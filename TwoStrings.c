77aca7bb5a4b444b5e81542b4e802930f1dab295
DivyaNanjilath <36256270+divyananjilath@users.noreply.github.com>
2020-08-01 12:10:06 +0530

    Update TwoStrings.c

--- TwoStrings.c
+++ TwoStrings.c
@@ -9,37 +9,30 @@
 #include <string.h>
 
 int main(){
-    int n,i,j,l1,l2,k,flag[100];
-    char s1[100],s2[100];
-    printf("n enter:");
+    int n,i,j,l1,l2,k,flag[20];
+    char s1[10000000],s2[10000000];
     scanf("%d",&n);
-	for(i=0;i<n;i++){flag[i]=0;}
     for(i=0;i<n;i++){
-	printf("s1 enter:");
         scanf("%s", s1);
-	printf("s2 enter:");
         scanf("%s", s2);
         j=0;
-	
-	while(s1[j] != '\0'){
-		k=0;
-		 while(s2[k] != '\0'){
-			if(s1[j]==s2[k]){
-				flag[i]=1;
-				break;			
-			}
-			k++;
-		}
-			j++;		
+    while(s1[j] != '\0'){
+        k=0;
+         while(s2[k] != '\0'){
+            if(s1[j]==s2[k]){
+                flag[i]=1;
+                break;            
+            }
+            k++;
+        }
+            j++;        
     }
     }
     for(i=0;i<n;i++){
-	if(flag[i]==1){
-		printf("YES\n");
-	}
-	else if(flag[i]==0){printf("NO\n");}
-}	
+    if(flag[i]==1){
+        printf("YES\n");
+    }
+    else {printf("NO\n");}
+}    
     return 0;
 }
-
-    
