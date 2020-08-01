dfbd4a72ec681b7ebb0401abde626e2e0c810849
DivyaNanjilath <36256270+divyananjilath@users.noreply.github.com>
2020-08-01 15:53:21 +0530

    Update TwoStrings.c

--- TwoStrings.c
+++ TwoStrings.c
@@ -1,35 +1,27 @@
-#include <assert.h>
-#include <limits.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdbool.h>
+bool check1(char s1[],char s2[]){
+    int i,flag[26]={0};
+    for(i=0;i<strlen(s1);i++){
+        flag[s1[i] - 'a']=1;
+    }
+    for(i=0;i<strlen(s2);i++){
+        if(flag[s2[i]-'a'] == 1){
+            return true;
+        };
+    }
+return false;
+}
 int main(){
-    int n,i,j,l1,l2,k,flag[20];
-    char s1[10000000],s2[10000000];
+    int n,i;
+    char s1[100000],s2[100000];
     scanf("%d",&n);
     for(i=0;i<n;i++){
         scanf("%s", s1);
         scanf("%s", s2);
-        j=0;
-    while(s1[j] != '\0'){
-        k=0;
-         while(s2[k] != '\0'){
-            if(s1[j]==s2[k]){
-                flag[i]=1;
-                break;            
-            }
-            k++;
-        }
-            j++;        
-    }
-    }
-    for(i=0;i<n;i++){
-    if(flag[i]==1){
+    if(check1(s1, s2)){
         printf("YES\n");
     }
     else {printf("NO\n");}
