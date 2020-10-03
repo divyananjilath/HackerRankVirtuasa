a2c4ed2e0ebbe3974569f35d7f9112b884414078
DivyaNanjilath <36256270+divyananjilath@users.noreply.github.com>
2020-10-03 20:24:33 +0530

    Update CountTriples.c

--- CountTriples.c
+++ CountTriples.c
@@ -1,29 +1,19 @@
-#include <assert.h>
-#include <ctype.h>
-#include <limits.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 int main(){
-    int a[1000000],n,r,i,k,count=0,j;
+    int a[1000000000],n,r,i,k,count=0,j;
     scanf("%d %d",&n,&r);
     for(i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
-    for(i=0;i<n;i++){
-        for(j=i+1;j<n;j++){
-            if(a[i]*r==a[j]){
-                for(k=j+1;k<n;k++){
-                    if(a[j]*r==a[k]){
-                            count++;
+    for(i=0;i<n-2;i++){
+        for(j=i+1;j<n-1;j++){
+            for(k=j+1;k<n;k++){
+                    if((a[i]*r==a[j]) && (a[j]*r==a[k])){
+                            count=count+1;
                     }
             }
         }
-    }}
+    }
     printf("%d",count);
     return 0;
 }
